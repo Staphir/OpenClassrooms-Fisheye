269fc8b53ef62f1cd0ce1a0a3481c5ae49a8513e
Maxime Dulieu <staphir@gmail.com>
2023-10-05 12:13:20 +0200

    homepage and link to photographer page with request GET

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,2 @@
-// Mettre le code JavaScript lié à la page photographer.html
+const params = new URL(document.location).searchParams;
+console.log(params.get('id'));
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,38 @@
 function photographerTemplate(data) {
-  const { name, portrait } = data;
+  const {
+    name, id, city, country, tagline, price, portrait,
+  } = data;
 
   const picture = `assets/photographers/${portrait}`;
+  const photographerPage = `./photographer.html?id=${id}`;
 
   function getUserCardDOM() {
     const article = document.createElement('article');
+    const photographerLink = document.createElement('a');
+    photographerLink.setAttribute('href', photographerPage);
+    photographerLink.setAttribute('aria-label', name);
     const img = document.createElement('img');
     img.setAttribute('src', picture);
+    img.setAttribute('alt', '');
     const h2 = document.createElement('h2');
     h2.textContent = name;
-    article.appendChild(img);
-    article.appendChild(h2);
+    const localisation = document.createElement('p');
+    localisation.className = 'localisation';
+    localisation.textContent = `${city}, ${country}`;
+    const description = document.createElement('p');
+    description.className = 'description';
+    description.textContent = tagline;
+    const amount = document.createElement('p');
+    amount.className = 'amount';
+    amount.textContent = `${price}€/jour`;
+    photographerLink.appendChild(img);
+    photographerLink.appendChild(h2);
+    article.appendChild(photographerLink);
+    article.appendChild(localisation);
+    article.appendChild(description);
+    article.appendChild(amount);
     return (article);
   }
+
   return { name, picture, getUserCardDOM };
 }
