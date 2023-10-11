5fa40b1a548f95d85a5900249fd9490ea18c2d40
Maxime Dulieu <staphir@gmail.com>
2023-10-11 14:02:53 +0200

    photographer page static components except inset like and price

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,2 +1,45 @@
-const params = new URL(document.location).searchParams;
-console.log(params.get('id'));
+async function getPhotographersDatas() {
+  try {
+    const response = await fetch('data/photographers.json');
+    const data = await response.json();
+    const params = new URL(document.location).searchParams;
+    const id = params.get('id');
+    const filterPhotographer = data.photographers.filter((item) => item.id === parseInt(id, 10));
+    const filterMedias = data.media.filter((item) => item.photographerId === parseInt(id, 10));
+    return ({ photographer: filterPhotographer[0], medias: filterMedias });
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log('Une erreur s\'est produite lors du chargement du fichier JSON:', error);
+    return null;
+  }
+}
+
+async function displayPhotographerHeader(photographer) {
+  const photographHeader = document.querySelector('.photograph-header');
+  const photographerModel = photographerTemplate(photographer);
+  const photographInformationsDOM = photographerModel.getUserInformationsDOM();
+  const photographImgDOM = photographerModel.getUserImgDOM();
+  photographHeader.insertBefore(photographInformationsDOM, photographHeader.children[0]);
+  photographHeader.appendChild(photographImgDOM);
+}
+
+async function displayPhotographerMedias(medias) {
+  // Attention mediatemplate boucle sur medias mais ne faire qu'une fois getSortDivDOM()
+  const gallerySection = document.querySelector('.gallery');
+  medias.forEach((media) => {
+    const mediaModel = mediaTemplate(media);
+    const mediaGallery = mediaModel.getUserGalleryDOM();
+    gallerySection.appendChild(mediaGallery);
+  });
+  // const sortDivDOM = mediaModel.getSortDivDOM();
+  // gallerySection.appendChild(sortDivDOM);
+}
+
+async function init() {
+// Récupère les datas des photographes
+  const { photographer, medias } = await getPhotographersDatas();
+  displayPhotographerHeader(photographer);
+  displayPhotographerMedias(medias);
+}
+
+init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -34,5 +34,118 @@ function photographerTemplate(data) {
     return (article);
   }
 
-  return { name, picture, getUserCardDOM };
+  function getUserInformationsDOM() {
+    const div = document.createElement('div');
+    div.id = 'photograph-informations';
+    const h1 = document.createElement('h1');
+    h1.textContent = name;
+    const localisation = document.createElement('p');
+    localisation.className = 'localisation';
+    localisation.textContent = `${city}, ${country}`;
+    const description = document.createElement('p');
+    description.className = 'description';
+    description.textContent = tagline;
+    div.appendChild(h1);
+    div.appendChild(localisation);
+    div.appendChild(description);
+    return div;
+  }
+
+  function getUserImgDOM() {
+    const img = document.createElement('img');
+    img.setAttribute('src', picture);
+    img.setAttribute('alt', name);
+    return img;
+  }
+
+  return {
+    name,
+    id,
+    city,
+    country,
+    tagline,
+    price,
+    portrait,
+    getUserCardDOM,
+    getUserInformationsDOM,
+    getUserImgDOM,
+  };
+}
+
+function mediaTemplate(data) {
+  const {
+    id, photographerId, title, image, video, likes, date, price,
+  } = data;
+
+  function getSortDivDOM() {
+    const div = document.createElement('div');
+    div.className = 'div-sort';
+    const label = document.createElement('label');
+    label.htmlFor = 'sort';
+    label.textContent = 'Trier par';
+    const select = document.createElement('select');
+    select.id = 'sort';
+    select.name = 'sort';
+    const popularityOption = document.createElement('option');
+    popularityOption.value = 'popularity';
+    popularityOption.textContent = 'Popularité';
+    const dateOption = document.createElement('option');
+    dateOption.value = 'date';
+    dateOption.textContent = 'Date';
+    const titleOption = document.createElement('option');
+    titleOption.value = 'title';
+    titleOption.textContent = 'Titre';
+    select.appendChild(popularityOption);
+    select.appendChild(dateOption);
+    select.appendChild(titleOption);
+    div.appendChild(label);
+    div.appendChild(select);
+    return div;
+  }
+
+  function getUserGalleryDOM() {
+    const divAllBlock = document.createElement('div');
+    divAllBlock.className = 'media';
+    if (image) {
+      // const figure = document.createElement('figure');
+      const img = document.createElement('img');
+      // changer Mimi en name
+      img.src = `assets/Sample Photos/Mimi/${image}`;
+      divAllBlock.appendChild(img);
+    } else {
+      const videoElement = document.createElement('video');
+      // changer Mimi en name
+      videoElement.src = `assets/Sample Photos/Ellie Rose/${video}`;
+      videoElement.controls = true;
+      divAllBlock.appendChild(videoElement);
+    }
+    const divTextMedia = document.createElement('div');
+    divTextMedia.className = 'divTextMedia';
+    const p = document.createElement('p');
+    p.textContent = title;
+    const divLikes = document.createElement('div');
+    // nombre de like chiffre dynamique
+    divLikes.textContent = 'xx';
+    const i = document.createElement('i');
+    i.classList.add('fa-solid');
+    i.classList.add('fa-heart');
+    i.ariaLabel = 'likes';
+    divLikes.appendChild(i);
+    divTextMedia.appendChild(p);
+    divTextMedia.appendChild(divLikes);
+    divAllBlock.appendChild(divTextMedia);
+    return divAllBlock;
+  }
+
+  return {
+    id,
+    photographerId,
+    title,
+    image,
+    likes,
+    date,
+    price,
+    getSortDivDOM,
+    getUserGalleryDOM,
+  };
 }
